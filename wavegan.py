b44342fbe6d5562690d05712dcf18f47aad3d7e4
Gasper Begus <35281438+gbegus@users.noreply.github.com>
2019-12-27 23:02:49 -0800

    Update wavegan.py

--- wavegan.py
+++ wavegan.py
@@ -251,3 +251,91 @@ def WaveGANDiscriminator(
   # Don't need to aggregate batchnorm update ops like we do for the generator because we only use the discriminator for training
 
   return output
+
+
+
+def WaveGANQ(
+    x,
+    kernel_len=25,
+    dim=64,
+    use_batchnorm=False,
+    phaseshuffle_rad=0,
+    num_categ=10):
+  batch_size = tf.shape(x)[0]
+  slice_len = int(x.get_shape()[1])
+
+  if use_batchnorm:
+    batchnorm = lambda x: tf.layers.batch_normalization(x, training=True)
+  else:
+    batchnorm = lambda x: x
+
+  if phaseshuffle_rad > 0:
+    phaseshuffle = lambda x: apply_phaseshuffle(x, phaseshuffle_rad)
+  else:
+    phaseshuffle = lambda x: x
+
+  # Layer 0
+  # [16384, 1] -> [4096, 64]
+  output = x
+  with tf.variable_scope('Qdownconv_0'):
+    output = tf.layers.conv1d(output, dim, kernel_len, 4, padding='SAME')
+  output = lrelu(output)
+  output = phaseshuffle(output)
+
+  # Layer 1
+  # [4096, 64] -> [1024, 128]
+  with tf.variable_scope('Qdownconv_1'):
+    output = tf.layers.conv1d(output, dim * 2, kernel_len, 4, padding='SAME')
+    output = batchnorm(output)
+  output = lrelu(output)
+  output = phaseshuffle(output)
+
+  # Layer 2
+  # [1024, 128] -> [256, 256]
+  with tf.variable_scope('Qdownconv_2'):
+    output = tf.layers.conv1d(output, dim * 4, kernel_len, 4, padding='SAME')
+    output = batchnorm(output)
+  output = lrelu(output)
+  output = phaseshuffle(output)
+
+  # Layer 3
+  # [256, 256] -> [64, 512]
+  with tf.variable_scope('Qdownconv_3'):
+    output = tf.layers.conv1d(output, dim * 8, kernel_len, 4, padding='SAME')
+    output = batchnorm(output)
+  output = lrelu(output)
+  output = phaseshuffle(output)
+
+  # Layer 4
+  # [64, 512] -> [16, 1024]
+  with tf.variable_scope('Qdownconv_4'):
+    output = tf.layers.conv1d(output, dim * 16, kernel_len, 4, padding='SAME')
+    output = batchnorm(output)
+  output = lrelu(output)
+
+  if slice_len == 32768:
+    # Layer 5
+    # [32, 1024] -> [16, 2048]
+    with tf.variable_scope('Qdownconv_5'):
+      output = tf.layers.conv1d(output, dim * 32, kernel_len, 2, padding='SAME')
+      output = batchnorm(output)
+    output = lrelu(output)
+  elif slice_len == 65536:
+    # Layer 5
+    # [64, 1024] -> [16, 2048]
+    with tf.variable_scope('Qdownconv_5'):
+      output = tf.layers.conv1d(output, dim * 32, kernel_len, 4, padding='SAME')
+      output = batchnorm(output)
+    output = lrelu(output)
+
+  # Flatten
+  output = tf.reshape(output, [batch_size, -1])
+
+  # Connect to single logit
+  with tf.variable_scope('Qoutput'):
+    Qoutput = tf.layers.dense(output, num_categ)
+
+
+  # Don't need to aggregate batchnorm update ops like we do for the generator because we only use the discriminator for training
+
+  return Qoutput
